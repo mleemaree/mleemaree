f14642b4dbd0b478c4f48a4164bda3232bd43cfe
Emily MaRee <lee@mleemaree.com>
2015-09-15 22:21:26 +0200

    first

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name mLeeMaReeApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the mLeeMaReeApp
+ */
+angular.module('mLeeMaReeApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name mLeeMaReeApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the mLeeMaReeApp
+ */
+angular.module('mLeeMaReeApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
